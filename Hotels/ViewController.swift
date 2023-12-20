066812971a3c0f37290a835c38f6a02870d5c912
Nurgali Nuraly <nurali.nurgali@gmail.com>
2023-12-20 18:04:56 +0600

    Initial Commit

---
+++ Hotels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotels
+//
+//  Created by Nurgali on 20.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
